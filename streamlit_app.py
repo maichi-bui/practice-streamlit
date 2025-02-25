152ac1edb0764393e724ea9842261e2790bf3ef7
maichi-bui <buimaichi.ctb.1611@gmail.com>
2025-02-25 15:31:58 +0100

    test plot train data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+def load_data_hourly():
+    data = pd.read_csv("plot_all.csv")
+    data['time'] = pd.to_datetime(data['time'])
+    
+    return data
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+data = load_data_hourly()
+fig = go.Figure()
+fig = px.line(data, x="time", y="ongoing_trips", color='date_snapshot',
+              markers=True, 
+              title="Scheduled Trips by Hour",
+              labels={"time": "Time (Hourly)", "ongoing_trips": "Number of On-going Trips"},
+              template="plotly_dark")
+fig.show()
