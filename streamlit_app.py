af8cbba367d0d996293f06510e277d88cfe1d682
Thimaichi Bui <r1014967@hercules.esat.kuleuven.be>
2025-02-26 23:30:07 +0100

    Commit for 2025-02-26 23:30:06

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,23 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from datetime import datetime, timedelta
 
 def load_data_hourly():
     data = pd.read_csv("plot_all.csv")
     data['time'] = pd.to_datetime(data['time'])
     
     recent_data = pd.read_csv("plot_df.csv")
+    recent_data.drop(columns="Unnamed: 0", inplace=True)
     recent_data['time'] = pd.to_datetime(recent_data['time'])
+    
     data = pd.concat([data, recent_data]).drop_duplicates()
-    data.to_csv("plot_all.csv")
-    return data
+    data.to_csv("plot_all.csv", index=False)
+    data['date_snapshot'] = data['date_snapshot'].apply(lambda x: datetime.strptime(str(x), "%Y%m%d").date())
+    
+    min_date = (datetime.now()- timedelta(7)).date()
+    plot_data = data[data.date_snapshot >= min_date]
+    return plot_data
 
 st.title("🚂 SNCB train data")
 # st.write(
