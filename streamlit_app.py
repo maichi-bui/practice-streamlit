9358d4128bb43ea224bdf50e3f6237f4ba7abf6c
Chi Bui <thimaichi.bui@student.kuleuven.be>
2025-03-08 01:17:31 +0100

    add delay chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,13 @@ def load_webcam_location():
 
     geo_data = pd.read_csv('webcam_location_info.csv')[['city','latitude','longitude','viewCount']]
     return geo_data
-# st.title("🚂 SNCB train data")
+
+def load_delay_data():
+    data = pd.read_csv('mean_delay.csv', parse_dates=['snap_time'])
+    min_date = datetime.now() - timedelta(7)
+    plot_data = data[data.snap_time >= min_date]
+    return plot_data
+
 # Sidebar for inputs
 with st.sidebar:
     st.write("### Selection for Graph")
@@ -41,13 +47,23 @@ with st.sidebar:
 #     "Test with train data"
 # )
 data = load_data_one_week()
-fig = go.Figure()
+delay_data = load_delay_data()
+
+# fig = go.Figure()
 fig = px.line(data, x="time", y="ongoing_trips", color='date_snapshot',
               markers=True, 
               title="Scheduled Trips by Hour",
               labels={"time": "Time (Hourly)", "ongoing_trips": "Number of On-going Trips"},
               template = 'seaborn')
             #   color_discrete_sequence=pastel_colors)
+fig_delay = go.Figure()
+
+# fig_delay = px.line(delay_data, x="snap_time", y="arrival_delay", template = 'seaborn',
+#                     title="Mean Delay per 15 minutes",
+#                     labels={"snap_time": "Time", "arrival_delay": "Mean Arrival Delay"}
+#                     )
+fig_delay.add_scatter(x=delay_data['snap_time'], y=delay_data['arrival_delay'], name='Mean Arrival Delay')
+fig_delay.add_scatter(x=delay_data['snap_time'], y=delay_data['departure_delay'], name='Mean Departure Delay')
 
 geo_data = load_webcam_location()
 fig2 = go.Figure()
@@ -58,6 +74,7 @@ fig2 = px.density_map(geo_data, lat='latitude', lon='longitude', z='viewCount', 
 if 'train' in selected_graph:
     st.title("🚂 SNCB train data")
     st.plotly_chart(fig, use_container_width=False)
+    st.plotly_chart(fig_delay, use_container_width=False)
 else:
     st.title("🗻 Webcam map")
     st.plotly_chart(fig2, use_container_width=False)
