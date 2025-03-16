82b9cddc46b00583d13092939e60f9a3dfde9b8e
Chi Bui <thimaichi.bui@student.kuleuven.be>
2025-03-16 23:30:08 +0100

    Commit for 2025-03-16 23:30:08

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
-# pastel_colors = ["#A1C4FD", "#FFC3A0", "#FF9A8B", "#D4A5A5", "#C1E1C1", "#F7CAC9", "#B5EAD7"]
-
-def update_new_data():
-    data = pd.read_csv("plot_all.csv")
-    data['time'] = pd.to_datetime(data['time'])
-    
-    recent_data = pd.read_csv("plot_df.csv")
-    recent_data.drop(columns="Unnamed: 0", inplace=True)
-    recent_data['time'] = pd.to_datetime(recent_data['time'])
-    
-    data = pd.concat([data, recent_data]).drop_duplicates()
-    data.to_csv("plot_all.csv", index=False)
-
 def load_data_one_week():
     
     data = pd.read_csv('plot_all.csv')
@@ -43,34 +30,30 @@ with st.sidebar:
     st.write("Select which graph you want like to see")
     selected_graph = st.selectbox("Select graph to display:", ('🚂 SNCB train data','🗻 Webcam location'))
 
-# st.write(
-#     "Test with train data"
-# )
+
 data = load_data_one_week()
 delay_data = load_delay_data()
 
-# fig = go.Figure()
+
 fig = px.line(data, x="time", y="ongoing_trips", color='date_snapshot',
               markers=True, 
               title="Scheduled Trips by Hour",
               labels={"time": "Time (Hourly)", "ongoing_trips": "Number of On-going Trips"},
               template = 'seaborn')
-            #   color_discrete_sequence=pastel_colors)
-fig_delay = go.Figure()
 
-# fig_delay = px.line(delay_data, x="snap_time", y="arrival_delay", template = 'seaborn',
-#                     title="Mean Delay per 15 minutes",
-#                     labels={"snap_time": "Time", "arrival_delay": "Mean Arrival Delay"}
-#                     )
+fig_delay = go.Figure()
 fig_delay.add_scatter(x=delay_data['snap_time'], y=delay_data['arrival_delay'], name='Mean Arrival Delay')
 fig_delay.add_scatter(x=delay_data['snap_time'], y=delay_data['departure_delay'], name='Mean Departure Delay')
+fig_delay.update_layout(title="Arrival and Departure Delay by Quarter-Hour in recent 7 days",
+                        xaxis_title="Time",
+                        yaxis_title="Delay (seconds)")
 
 geo_data = load_webcam_location()
 fig2 = go.Figure()
 fig2 = px.density_map(geo_data, lat='latitude', lon='longitude', z='viewCount', radius=10,
                         center=dict(lat=50, lon=4),zoom=5,color_continuous_scale="Viridis",
                         map_style="streets")
-# fig.show()
+
 if 'train' in selected_graph:
     st.title("🚂 SNCB train data")
     st.plotly_chart(fig, use_container_width=False)
